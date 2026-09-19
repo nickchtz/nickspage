28736fedb63abab8bac942ae793a10e54e1253cf
Nick Chatzikos <chatzikos@proton.me>
2026-09-19 10:40:28 +0300

    Implement 404 page & Correct TikTok link issue

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,7 +1,7 @@
 export const links = [
     { icon: './app/img/github.png', text: 'GitHub', url: 'https://github.com/nickchtz.com' },
     { icon: './app/img/instagram.png', text: 'Instagram', url: 'https://instagram.com/nickdotphp' },
-    { icon: './app/img/tiktok.png', text: 'TikTok', url: 'https://tiktok.com/nick.tsx' },
+    { icon: './app/img/tiktok.png', text: 'TikTok', url: 'https://tiktok.com/@nick.tsx' },
     { icon: './app/img/envelope-solid.png', text: 'Email Address', url: '' },
     { icon: './app/img/arrow-left.png', text: 'Main Site', url: 'https://nickchtz.com' },
 ];
